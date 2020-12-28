62759d4f04fde9b0879baf5a4e3bb93e724efa83
Ubuntu <ubuntu@ubuntu.station>
2020-12-28 13:38:09 +0000

    Error corrections, deleted ' char on queries.csv

--- allcars.py
+++ allcars.py
@@ -8,36 +8,40 @@ import time
 from classes import PeerOrg1Thread, AllCarsThread, PeerENV_Org1, DockerThread
 
 def main():
-	home_dir = os.environ['HOME']
-	TEST_NETWORK=f'{home_dir}/fabric-samples/test-network'
-	OLD_PATH=os.environ['PATH']
-	os.chdir(TEST_NETWORK)
+    home_dir = os.environ['HOME']
+    TEST_NETWORK=f'{home_dir}/fabric-samples/test-network'
+    OLD_PATH=os.environ['PATH']
+    os.chdir(TEST_NETWORK)
 
-#Setting common env variables
-# $PATH=TEST_NETWORK+'/../bin:'+OLD_PATH
-# $FABRIC_CFG_PATH=TEST_NETWORK+'/../config/'
+    #Setting common env variables
+    # $PATH=TEST_NETWORK+'/../bin:'+OLD_PATH
+    # $FABRIC_CFG_PATH=TEST_NETWORK+'/../config/'
 
-	os.environ['PATH']=TEST_NETWORK+'/../bin:'+OLD_PATH
-	os.environ['FABRIC_CFG_PATH']=TEST_NETWORK+'/../config/'
+    os.environ['PATH']=TEST_NETWORK+'/../bin:'+OLD_PATH
+    print(f"Set PATH: {os.environ['PATH']}")
+    os.environ['FABRIC_CFG_PATH']=TEST_NETWORK+'/../config/'
+    print(f"Set FABRIC_CFG_PATH: {os.environ['FABRIC_CFG_PATH']}")
 
-#Setting Peer env variables
-#Test all cars
+    #Setting Peer env variables
+    #Test all cars
 
-	env_org1 = PeerENV_Org1(TEST_NETWORK)
+    env_org1 = PeerENV_Org1(TEST_NETWORK)
 
 
-	for k, v in env_org1.__dict__.items():
-		os.environ[k]=v
+    for k, v in env_org1.__dict__.items():
+        os.environ[k]=v
+        print(f"Set {k}: {v}")
 
-	allcars = AllCarsThread(TEST_NETWORK)
-	allcars.start()
-	allcars.join()
+    allcars = AllCarsThread(TEST_NETWORK)
+    print("Starting allCars query")
+    allcars.start()
+    allcars.join()
 
-	if allcars.stdout:
-		print(allcars.stdout.decode('utf-8'))
-		print("\nNumber of cars: %d" % (allcars.stdout.decode('utf-8').count('Key') ) )
-	else:
-		print(allcars.stderr.decode('utf-8'))
+    if allcars.stdout:
+        print(allcars.stdout.decode('utf-8'))
+        print("\nNumber of cars: %d" % (allcars.stdout.decode('utf-8').count('Key') ) )
+    else:
+        print(allcars.stderr.decode('utf-8'))
 
 if __name__ == "__main__":
-	main()
+    main()
--- main.py
+++ main.py
@@ -9,127 +9,143 @@ from classes import PeerOrg1Thread, PeerENV_Org1, DockerThread
 import argparse
 
 parser = argparse.ArgumentParser(description='Hyperledger Fabric test-network query test')
-parser.add_argument('num_queries', type=int, help='Number of queries (0<n<=250000', const=10, default=10, nargs='?')
+parser.add_argument('num_queries', type=int, help='Number of queries (0<n<=250000', const=2, default=2, nargs='?')
 args = parser.parse_args()
 
-def done_filter(x, done):
-	for y in done:
-		if x[0]==y[0]:
-			return True
-	return False
+def done_filter(rows, done):
+
+    tmp = list( filter( lambda x: x[0] in [y[0] for y in done], rows) )
+    for k in tmp:
+        ind=None
+        for i, x in enumerate(rows):
+            if k[0]==x[0]:
+                ind=i
+                break
+        if ind:
+            del rows[ind]
+            ind=None
+
+def save_outputs(PWD, docker, exec_times, done):
+    try:
+        old_pwd = os.environ['PWD']
+        os.chdir(PWD)
+        with open("docker-stats_log.txt", "w") as output:
+            output.writelines(docker)
+        with open("execution-times.csv", "w") as output:
+            for i,t in enumerate(exec_times):
+                output.write('%d,%.3f\n' % (i,t))
+        with open("done.csv", "w") as output:
+            csv_writer = csv.writer(output, delimiter=',')
+            
+            for row in done:
+                csv_writer.writerow(row)
+
+        os.chdir(old_pwd)
+    except:
+        traceback.format_exc()
+        sys.exit(1)
 
 def main():
-	logger = DockerThread()
-	logger.start()
-	print("Started Docker logging thread")
+    logger = DockerThread()
+    logger.start()
+    print("Started Docker logging thread")
 
-	exec_times = []
+    exec_times = []
 
-	done = []
+    done = []
 
-#Loading committed queries
+    #Loading committed queries
 
-	try:
-		with open('done.csv', mode='r') as csv_file:
-			csv_reader = csv.reader(csv_file, delimiter=',')
+    try:
+        with open('done.csv', mode='r') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
 
-			for l in csv_reader:
-				done.append(l)
-			print("Found committed queries file")
+            for l in csv_reader:
+                done.append(l)
+            print("Found committed queries file")
 
-	except FileNotFoundError:
-		print("Not found committed queries file")
-		pass
+    except FileNotFoundError:
+        print("Not found committed queries file")
+        pass
 
 
     #Load queries file
 
-	try:
-		with open('queries.csv', mode='r') as csv_file:
-			csv_reader = csv.reader(csv_file, delimiter=',')
-			rows = []
-			for l in csv_reader:
-				rows.append(l)
+    try:
+        with open('queries.csv', mode='r') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            rows = []
+            for l in csv_reader:
+                rows.append(l)
 
-		del rows[-1]	
-
-		rows = list( filter( lambda x: done_filter(x, done), rows) )
-
-		print("Loaded queries file in memory")
-	except :
-		traceback.print_exc()
-		sys.exit(1)
-
-	home_dir = os.environ['HOME']
-	TEST_NETWORK=f'{home_dir}/fabric-samples/test-network'
-	OLD_PATH=os.environ['PATH']
-	OLD_PWD=os.environ['PWD']
+            done_filter(rows, done)
+        
+            
+        if len(rows)==0:
+            print("There are no more queries")
+            sys.exit(1)
 
 
-	os.chdir(TEST_NETWORK)
-	print(f"Changed directory to TEST_NETWORK: {TEST_NETWORK}")
+        print("Loaded queries file in memory")
+    except :
+        traceback.print_exc()
+        sys.exit(1)
+
+    home_dir = os.environ['HOME']
+    TEST_NETWORK=f'{home_dir}/fabric-samples/test-network'
+    OLD_PATH=os.environ['PATH']
+    OLD_PWD=os.environ['PWD']
 
 
-#Setting common env variables
-# $PATH=TEST_NETWORK+'/../bin:'+OLD_PATH
-# $FABRIC_CFG_PATH=TEST_NETWORK+'/../config/'
+    os.chdir(TEST_NETWORK)
+    print(f"Changed directory to TEST_NETWORK: {TEST_NETWORK}")
 
-	os.environ['PATH']=TEST_NETWORK+'/../bin:'+OLD_PATH
-	print(f"Set PATH: {os.environ['PATH']}")
-	os.environ['FABRIC_CFG_PATH']=TEST_NETWORK+'/../config/'
-	print(f"Set FABRIC_CFG_PATH: {os.environ['FABRIC_CFG_PATH']}")
-#Setting Peer env variables
-#Test1
-	env_org1 = PeerENV_Org1(TEST_NETWORK)
 
-	for k, v in env_org1.__dict__.items():
-		os.environ[k]=v
-		print(f"Set {k}: {v}")
+    #Setting common env variables
+    # $PATH=TEST_NETWORK+'/../bin:'+OLD_PATH
+    # $FABRIC_CFG_PATH=TEST_NETWORK+'/../config/'
 
-	count = 0
+    os.environ['PATH']=TEST_NETWORK+'/../bin:'+OLD_PATH
+    print(f"Set PATH: {os.environ['PATH']}")
+    os.environ['FABRIC_CFG_PATH']=TEST_NETWORK+'/../config/'
+    print(f"Set FABRIC_CFG_PATH: {os.environ['FABRIC_CFG_PATH']}")
+    #Setting Peer env variables
+    #Test1
+    env_org1 = PeerENV_Org1(TEST_NETWORK)
 
-	try:
+    for k, v in env_org1.__dict__.items():
+        os.environ[k]=v
+        print(f"Set {k}: {v}")
 
-		for row in rows:
+    count = 0
 
-			if count >= args.num_queries:
-				break
+    try:
 
-			peer = PeerOrg1Thread(PWD=TEST_NETWORK, query=row)
-			peer.start()
-			peer.join()
-			exec_times.append(peer.execTime)
-			print(f'Added {row} to the Ledger')
-			time.sleep(1)
-			count += 1
+        for row in rows[1:]:
 
-			done.append(row)
-	except:
-		traceback.format_exc()
+            if count >= args.num_queries:
+                break
 
-	finally:
-		logger.stop()
-		logger.join()
-		print("Stopped Docker logging thread")
-		save_outputs(PWD=OLD_PWD, docker=logger.outputs, exec_times=exec_times, done=done)
-		print(f"Saved outputs in: {OLD_PWD}")
+            peer = PeerOrg1Thread(PWD=TEST_NETWORK, query=row)
+            peer.start()
+            peer.join()
+            exec_times.append(peer.execTime)
+            print(f'Added {row} to the Ledger')
+            print(f'Query n. {count+1}')
+            time.sleep(1)
+            count += 1
 
-def save_outputs(PWD, docker, exec_times, done):
-	try:
-		old_pwd = os.environ['PWD']
-		os.chdir(PWD)
-		with open("docker-stats_log.txt", "w") as output:
-			output.writelines(docker)
-		with open("execution-times.csv", "w") as output:
-			for i,t in enumerate(exec_times):
-				output.write('%d,%.3f\n' % (i,t))
-		with open("done.csv", "w") as output:
-			output.writelines(done)
+            done.append(row)
+    except:
+        traceback.format_exc()
 
-		os.chdir(old_pwd)
-	except:
-		traceback.format_exc()
-		sys.exit(1)
+    finally:
+        logger.stop()
+        logger.join()
+        print("Stopped Docker logging thread")
+        save_outputs(OLD_PWD, logger.outputs, exec_times, done)
+        print(f"Saved outputs in: {OLD_PWD}")
+
 
 if __name__ == "__main__":
-	main()
+    main()
